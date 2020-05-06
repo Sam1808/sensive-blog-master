480378592fa4c40ba87c434eff1bf6ba75537c0a
Anton Pozdnyakov <anton.pozdnyakov.i@gmail.com>
2020-05-06 14:16:46 +0300

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from blog.models import Post, Tag, Comment
+
+class PostAdmin(admin.ModelAdmin):
+    raw_id_fields = ('likes', 'tags')
+
+class CommentAdmin(admin.ModelAdmin):
+    raw_id_fields = ('post','author')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
